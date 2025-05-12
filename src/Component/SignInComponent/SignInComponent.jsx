72478d46f78f444bd9544b45c9340dbb11b140e8
kienld <laiduckien28@gmail.com>
2025-05-12 13:34:11 +0700

    Reponsive for ui signin, signup

--- src/Component/SignInComponent/SignInComponent.jsx
+++ src/Component/SignInComponent/SignInComponent.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { Alert } from 'antd';
+import { size } from 'lodash';
 
 const SignInComponent = () => {
   const navigate = useNavigate()
@@ -74,7 +75,118 @@ const SignInComponent = () => {
     
 
     <div className="absolute top-50 w-full" > 
-    <Row>
+    <div className='block sm:hidden'>     <Row>
+      <Col span={1}></Col>
+      <Col span={22} >
+      {
+        mutation.isSuccess ? ( 
+          <div className='mb-10'> 
+      
+      <Alert
+      message="Đăng nhập thành công"
+      description="Đang chuyển hướng sang trang chủ"
+      type="success"
+      showIcon
+    />
+      </div>
+        ) : null
+      }
+
+      {
+        mutation.isError ? ( 
+          <div className='mb-10'> 
+      
+      <Alert
+      message="Đăng kí không thành công"
+      description="Vui Lòng Kiểm Tra Lại Thông Tin Đăng NhậpNhập "
+      type="error"
+      showIcon
+    />
+      </div>
+        ) : null
+      }
+      <div className='flex border border-gray-100 shadow-2xl rounded-sm p-3 bg-white justify-between relative'> 
+
+      <div className="absolute -top-4 -right-4 text-gray-500  text-3xl p-1 cursor-pointer z-10"
+      onClick={() => {
+        backtohome()
+      }}>
+
+<CloseCircleFilled/>
+</div>
+      <div className='flex-2'> 
+      <div> 
+        <p className='font-bold text-[13px]'> Xin chào</p>
+        <p className='text-[12px]'> Đăng nhập hoặc Tạo tài khoản</p>
+      </div>
+
+      <div className='py-1'> 
+      <Input placeholder="Email của bạn" onChange={(e) => {
+        setemail(e.target.value) 
+      }} size='small'/>
+      </div>
+      <div className='py-1'> 
+      <Input placeholder="Mật khâủ" 
+        onChange={(e) => {
+
+          setpassword(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-2 items-center'> 
+      
+        <button className='border border-red-600 bg-red-600 text-white px-1 py-1 rounded text-[13px]'
+        onClick={() => {
+          mutation.mutate({
+            email: email,
+            password: password
+          })
+        }
+        }
+
+        
+
+        
+        > 
+        Đăng Nhập
+        </button>
+      </div>
+      <div className='flex text-[13]'> 
+      <p> Quên mật khẩu?</p>
+      </div>
+      <div className='flex text-[13] gap-2'> 
+      <p> Bạn chưa có tài khoản?</p>
+      <p className='text-red-600 cursor-pointer' onClick={() => {
+        signup()
+      }}> Đăng Kí </p>
+      </div>
+
+      <div> 
+      <p className='flex text-[9px]'> 
+      Bằng việc tiếp tục, bạn đã đọc và đồng ý với điều khoản sử dụng và Chính sách bảo mật thông tin cá nhân của Tiki
+      </p>
+      </div>
+      
+      </div>
+
+
+      <div className='flex-1'> 
+      
+      <img src='https://salt.tikicdn.com/ts/upload/df/48/21/b4d225f471fe06887284e1341751b36e.png'/>
+      </div>
+
+
+
+
+      </div>
+
+
+      </Col>
+      <Col span={1}></Col>
+    </Row></div>
+
+
+        <div className='hidden sm:block'>     <Row>
       <Col span={10}></Col>
       <Col span={8} >
       {
@@ -182,7 +294,7 @@ const SignInComponent = () => {
 
       </Col>
       <Col span={4}></Col>
-    </Row>
+    </Row></div>
     </div>
 
 
--- src/Component/SignUpComponent/SignUpComponent.jsx
+++ src/Component/SignUpComponent/SignUpComponent.jsx
@@ -27,14 +27,6 @@ const SignUpComponent = () => {
   })
 
 
-
-
-
-
-  // const data_raw = mutation.data.data.message;
-  // const isSuccess = mutation.isSuccess
-  // const isLoading = mutation.isLoading
-
   const [ username, setusername ] = useState('')
   const [ password, setpassword ] = useState('')
   const [ email, setemail ] = useState('')
@@ -63,14 +55,6 @@ const SignUpComponent = () => {
 
   return (
     <div className="relative" >
-      {/* {mutation.isLoading && (
-        <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-70 z-50 flex items-center justify-center">
-          <Spin tip="Đang xử lý..." size="large" />
-        </div>
-      )} */}
-
-
-
 
     <div > 
         <HomePage/>
@@ -78,8 +62,153 @@ const SignUpComponent = () => {
 
     
 
-    <div className="absolute top-50 w-full " > 
-    <Row>
+    <div className="absolute top-50  " > 
+
+    <div className='block sm:hidden'>     <Row>
+
+      <Col span={1}></Col>
+      
+      <Col span={22} >
+
+      {
+        mutation.isSuccess ? ( 
+          <div className='mb-10'> 
+      
+      <Alert
+      message="Đăng kí thành công"
+      description="Đang chuyển hướng sang màn hình đăng nhập"
+      type="success"
+      showIcon
+    />
+      </div>
+        ) : null
+      }
+
+      {
+        mutation.isError ? ( 
+          <div className='mb-10'> 
+      
+      <Alert
+      message="Đăng kí khôngkhông thành công"
+      description="Vui Lòng Kiểm Tra Lại Thông Tin Đăng Kí "
+      type="error"
+      showIcon
+    />
+      </div>
+        ) : null
+      }
+
+
+
+      <div className='flex border border-gray-100 shadow-sm rounded-xl  bg-white justify-between relative p-3'> 
+
+      <div className="absolute -top-4 -right-4 text-gray-500  text-3xl p-1 cursor-pointer z-10"
+      onClick={() => {
+        backtohome()
+      }}>
+
+<CloseCircleFilled/>
+</div>
+      <div className='flex-2'> 
+      <div> 
+        <p className='font-bold text-[13px]'> Xin chào</p>
+        <p className='text-[12px]'> Đăng nhập hoặc Tạo tài khoản</p>
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Họ tên" 
+        onChange={(e) => {
+          setusername(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Số điện thoại" 
+                onChange={(e) => {
+                  setphone(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Email của bạn" 
+              onChange={(e) => {
+          setemail(e.target.value)
+        }} size='small' /> 
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Địa chỉ" 
+                      onChange={(e) => {
+          setaddress(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Mật khâủ" 
+        onChange={(e) => {
+          setpassword(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-1 text-[10px]'> 
+      <Input placeholder="Xác Nhận Mật Khẩu" 
+        onChange={(e) => {
+          setcfpassword(e.target.value)
+        }} size='small'
+      />
+      </div>
+      <div className='py-2 items-center'> 
+      
+        <button className='border border-red-600 bg-red-600 text-white px-1 py-1 rounded text-[13px]'
+        onClick={() => {
+          mutation.mutate(
+          {
+            name: username,
+            password: password,
+            email: email,
+            phone: phone,
+            address: address,
+            confirmpassword: confirmpassword
+          }
+        ),
+        console.log("mutation", mutation)
+        
+        
+        }}> 
+        Đăng Kí
+        </button>
+      </div>
+
+      <div className='flex text-[12px] gap-1'> 
+      <p> Bạn đã có tài khoản?</p>
+      <p className='text-red-500 font-bold cursor-pointer text-[12px]' onClick={() => {
+          login()
+          
+      }}> Đăng Nhập </p>
+      </div>
+
+
+      
+      </div>
+
+
+      <div className='flex-1'> 
+      
+      <img src='https://salt.tikicdn.com/ts/upload/df/48/21/b4d225f471fe06887284e1341751b36e.png'/>
+      </div>
+
+
+
+
+      </div>
+
+
+      </Col>
+      <Col span={1}></Col>
+    </Row></div>
+
+
+
+
+    <div className='hidden sm:block'>    <Row>
 
       <Col span={10}></Col>
       
@@ -218,7 +347,9 @@ const SignUpComponent = () => {
 
       </Col>
       <Col span={4}></Col>
-    </Row>
+    </Row> </div>
+
+
     </div>
 
 
